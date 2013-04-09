dde16ec67a7c1a6a9731acd468588235627376b6
Alejandro Alcalde <algui91@gmail.com>
2013-04-09 09:35:26 +0200

    First commit
    Creating the project

---
+++ Debug/src/pivot.d
@@ -0,0 +1,1 @@
+src/pivot.d: ../src/pivot.cpp
---
+++ src/pivot.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : pivot.cpp
+// Author      : Alejandro Alcalde <algui91@gmail.com>
+// Version     :
+// Copyright   : GPLv3
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "GNM" << endl; // prints GNM
+	return 0;
+}
