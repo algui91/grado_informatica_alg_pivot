4b427d28568663a232b1c4953037418093260c1c
Alejandro Alcalde <algui91@gmail.com>
2013-04-16 21:13:58 +0200

    Measuting execution time

--- src/pivot.cpp
+++ src/pivot.cpp
@@ -10,8 +10,11 @@
 #include <vector>
 #include <algorithm>
 #include <math.h>
+#include <sys/time.h>
 
-#define SIZE 9
+#define SIZE 1000000
+#define MEASURE_PIVOT 1 /* Print execution time in us */
+#define PRINT_VECTOR 0	/* Set to true for print the array's reordering */
 
 using namespace std;
 
@@ -19,7 +22,7 @@ vector<int> A(SIZE, 0);
 
 int
 getRandom() {
-	return rand() % 20;
+	return rand() % 1024;
 }
 
 void
@@ -69,7 +72,9 @@ select(int s){
 
 	do {
 		pivot(i, j, l);
+#if PRINT_VECTOR
 		printArray();
+#endif
 		if (s < l)
 			j = l - 1;
 		else if (s > l)
@@ -81,12 +86,37 @@ select(int s){
 }
 
 int
+crono(int (*func)(int), int s) {
+	struct timeval tv1, tv2; /* gettimeofday() secs-usecs */
+	long tv_usecs; /* y sus cuentas */
+
+	gettimeofday(&tv1, NULL);
+	int result = func(s);
+	gettimeofday(&tv2, NULL);
+
+	tv_usecs = (tv2.tv_sec - tv1.tv_sec) * 1E6
+	                + (tv2.tv_usec - tv1.tv_usec);
+
+	cout << "Measured execution time " << tv_usecs << " us" << endl;
+
+	return result;
+}
+
+int
 main() {
 	srand(time(NULL));
 	populateVector();
 	cout << "Original Vector: "; printArray();
 	int s = ceil(SIZE/2);
-	cout << "Median of A: " << select(s) << endl;
+	int resultado;
+
+#if MEASURE_PIVOT
+	resultado = crono(select, s);
+#else
+	resultado = select(s);
+#endif
+
+	cout << "Median of A: " << resultado;
 
 	return 0;
 }
